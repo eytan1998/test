d248bdc6a6ed7848e22d0c3ac5223eb1b5b676ad
eytan1998 <eytan1998@gmail.com>
2022-11-15 09:02:49 +0200

    change main hander nagitive and to match test

--- main.c
+++ main.c
@@ -3,27 +3,52 @@
 #include "NumClass.h"
 
 int main() {
-    int start, end;
-    scanf("%d%d", &start, &end);
-    printf("The Armstrong numbers are:");
-    for (int i = start; i < end; i++) {
-        if (isArmstrong(i))
-            printf(" %d", i);
+    int start;
+    int end;
+    scanf("%d", &start);
+    scanf("%d", &end);
+    if (start <= 0 || end <= 0){
+        printf("error: numbers must be positive integer\n");
+        return 1;
     }
-    printf("\nThe Palindromes are:");
+    int flagToPrint = 1;
 
     for (int i = start; i < end; i++) {
-        if (isPalindrome(i))
+        if (isArmstrong(i)) {
+            if (flagToPrint) {
+                printf("The Armstrong numbers are:");
+                flagToPrint = 0;
+            }
             printf(" %d", i);
+        }
     }
-    printf("\nThe Prime numbers are:");
+    flagToPrint = 1;
     for (int i = start; i < end; i++) {
-        if (isPrime(i))
+        if (isPalindrome(i)) {
+            if (flagToPrint) {
+                printf("\nThe Palindromes numbers are:");
+                flagToPrint = 0;
+            }
             printf(" %d", i);
+        }
     }
-    printf("\nThe Strong numbers are:");
+    flagToPrint = 1;
+    for (int i = start; i < end; i++) {
+        if (isPrime(i)) {
+            if (flagToPrint) {
+                printf("\nThe Prime numbers are:");
+                flagToPrint = 0;
+            }
+            printf(" %d", i);
+        }
+    }
+    flagToPrint = 1;
     for (int i = start; i < end; i++) {
         if (isStrong(i)) {
+            if (flagToPrint) {
+                printf("\nThe Strong numbers are:");
+                flagToPrint = 0;
+            }
             printf(" %d", i);
         }
     }
