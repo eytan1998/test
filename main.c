43e8c3b9ef5290a6f6d56df94ddb1869812cf4c6
eytan1998 <eytan1998@gmail.com>
2022-11-15 16:20:03 +0200

    change main match tester#2

--- main.c
+++ main.c
@@ -8,48 +8,34 @@ int main() {
     int end;
     scanf("%d", &start);
     scanf("%d", &end);
-    if (start <= 0 || end <= 0){
+    if (start <= 0 || end <= 0) {
         printf("error: numbers must be positive integer\n");
         return 1;
     }
-    int flagToPrint = 1;
 
+    printf("The Armstrong numbers are:");
     for (int i = start; i < end; i++) {
         if (isArmstrong(i)) {
-            if (flagToPrint) {
-                printf("The Armstrong numbers are:");
-                flagToPrint = 0;
-            }
             printf(" %d", i);
         }
     }
-    flagToPrint = 1;
+    printf("\nThe Palindromes are:");
     for (int i = start; i < end; i++) {
         if (isPalindrome(i)) {
-            if (flagToPrint) {
-                printf("\nThe Palindromes are:");
-                flagToPrint = 0;
-            }
             printf(" %d", i);
         }
     }
-    flagToPrint = 1;
+
+    printf("\nThe Prime numbers are:");
     for (int i = start; i < end; i++) {
         if (isPrime(i)) {
-            if (flagToPrint) {
-                printf("\nThe Prime numbers are:");
-                flagToPrint = 0;
-            }
             printf(" %d", i);
         }
+
     }
-    flagToPrint = 1;
+    printf("\nThe Strong numbers are:");
     for (int i = start; i < end; i++) {
         if (isStrong(i)) {
-            if (flagToPrint) {
-                printf("\nThe Strong numbers are:");
-                flagToPrint = 0;
-            }
             printf(" %d", i);
         }
     }
